f16e0b76edfd759e66bd897b13cecf0b3a8a0437
azkiarifa <azkiarifaarsyana@gmail.com>
2025-10-23 17:55:06 +0700

    push -azkia

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,10 @@ Route::get('/penjual/dashboard', [penjualController::class, 'dashboard'])->name(
 Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
 Route::post('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
 
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
+Route::get('/products/edit/{id}', [ProductController::class, 'edit'])->name('products.edit');
+Route::put('/products/{id}', [ProductController::class, 'update'])->name('products.update');
+Route::get('/products/delete/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
+
